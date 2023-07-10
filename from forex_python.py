6b30bc2ec8eeb3bfdbe70cf2f896901d1df0327a
millsdev-ai <mills.adu@yahoo.com>
2023-07-10 01:02:04 -0700

    Added comments

---
+++ from forex_python.py
@@ -0,0 +1,17 @@
+from forex_python.converter import CurrencyRates
+
+#Create an object of CurrencyRates class
+c = CurrencyRates()
+
+#Get the amount and currency
+amount = int(input("Enter amount: "))
+from_currency = input("From Currency: ").upper()
+to_currency = input("To Currency: ").upper()
+
+#Prints the currency conversion
+print(from_currency, " To ", to_currency, amount)
+result = c.convert(from_currency, to_currency, amount) 
+
+print(result)
+
+#millsdev-ai
